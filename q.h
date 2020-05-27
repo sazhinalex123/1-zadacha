8e1c94ab11ca68f87e1bbbc647d8f04898c3610f
sazhinalex123 <66011595+sazhinalex123@users.noreply.github.com>
2020-05-27 13:59:22 +0300

    Create q.h

---
+++ q.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <math.h>
+double  sarf(int**arr,int i, int a, int b);
+int search(int **arr,int a,int b);
+void pechat_1(int** arr,int a,int b);
+void pechat_2(int** arr,int a,int b);
+int ff(const char* filename);
+void autotest(void);
